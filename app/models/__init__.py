5f549d30c3d97cc2ed421f02a883fd45d0417c08
alexliu2360 <alexliu2360@126.com>
2018-10-17 16:59:52 +0800

    Create __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+# ------------------------------------------------------------
+#  版本：0.1
+#  版权：****
+#  模块：****
+#  功能：****
+#  语言：Python3.6
+#  作者：****<****@aconbot.com.cn>
+#  日期：2018-07-18
+# ------------------------------------------------------------
+#  修改人：****<****@aconbot.com.cn>
+#  修改日期：2018-07-18
+#  修改内容：创建
+# ------------------------------------------------------------
+from .engine import sentiment_submit_manager
+from .engine import sentiment_train_manager
+
+
+__all__ = ['sentiment_submit_manager', 'sentiment_train_manager']
